f278e358bba10a95f80eeb441094b12aacc241d4
Guilherme Brito <guilhermebritoalmeidacosta@gmail.com>
2023-09-29 21:24:25 -0300

    Where, Order By e Join statements conversors

--- FetchXMLToSql.cs
+++ FetchXMLToSql.cs
@@ -4,8 +4,11 @@ using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Xml;
 using System.Xml.Serialization;
+using static System.Net.WebRequestMethods;
 
 namespace FetchXMLToSql
 {
@@ -15,9 +18,11 @@ namespace FetchXMLToSql
         [XmlRoot(ElementName = "attribute")]
         private class Attribute
         {
-
             [XmlAttribute(AttributeName = "name")]
             public string Name { get; set; }
+
+            [XmlAttribute(AttributeName = "alias")]
+            public string Alias { get; set; }
         }
 
         [XmlRoot(ElementName = "condition")]
@@ -37,6 +42,52 @@ namespace FetchXMLToSql
             public string Text { get; set; }
         }
 
+        [XmlRoot(ElementName = "filter")]
+        private class Filter
+        {
+
+            [XmlElement(ElementName = "condition")]
+            public List<Condition> Condition { get; set; }
+
+            [XmlAttribute(AttributeName = "type")]
+            public string Type { get; set; } // And & Or
+
+            [XmlElement(ElementName = "filter")]
+            public Filter InnerFilter { get; set; }
+
+            [XmlText]
+            public string Text { get; set; }
+        }
+
+        [XmlRoot(ElementName = "link-entity")]
+        private class LinkEntity
+        {
+
+            [XmlElement(ElementName = "attribute")]
+            public List<Attribute> Attribute { get; set; }
+
+            [XmlAttribute(AttributeName = "name")]
+            public string Name { get; set; }
+
+            [XmlAttribute(AttributeName = "from")]
+            public string From { get; set; }
+
+            [XmlAttribute(AttributeName = "to")]
+            public string To { get; set; }
+
+            [XmlAttribute(AttributeName = "visible")]
+            public bool Visible { get; set; }
+
+            [XmlAttribute(AttributeName = "link-type")]
+            public string LinkType { get; set; }
+
+            [XmlAttribute(AttributeName = "alias")]
+            public string Alias { get; set; }
+
+            [XmlElement(ElementName = "filter")]
+            public List<Filter> Filter { get; set; }
+        }
+
         [XmlRoot(ElementName = "order")]
         private class Order
         {
@@ -46,25 +97,6 @@ namespace FetchXMLToSql
 
             [XmlAttribute(AttributeName = "descending")]
             public string Descending { get; set; }
-        }
-
-        [XmlRoot(ElementName = "filter")]
-        private class Filter
-        {
-
-            [XmlElement(ElementName = "condition")]
-            public List<Condition> Condition { get; set; }
-
-            [XmlAttribute(AttributeName = "type")]
-            public string Type { get; set; }
-
-            // And & Or
-
-            [XmlText]
-            public string Text { get; set; }
-
-            [XmlElement(ElementName = "filter")]
-            public Filter InnerFilter { get; set; }
         }
 
         [XmlRoot(ElementName = "entity")]
@@ -83,8 +115,14 @@ namespace FetchXMLToSql
             [XmlElement(ElementName = "filter")]
             public Filter Filter { get; set; }
 
+            [XmlElement(ElementName = "link-entity")]
+            public List<LinkEntity> LinkEntity { get; set; }
+
             [XmlAttribute(AttributeName = "name")]
             public string Name { get; set; }
+
+            [XmlAttribute(AttributeName = "alias")]
+            public string Alias { get; set; }
 
             [XmlText]
             public string Text { get; set; }
@@ -119,129 +157,13 @@ namespace FetchXMLToSql
             public string Text { get; set; }
         }
 
-        #region [Enums]
-        private enum Operators
-        {
-            [Description("lt")]
-            LessThan,
-            [Description("gt")]
-            GreaterThan,
-            [Description("le")]
-            LessThanorEqualTo,
-            [Description("ge")]
-            GreaterThanorEqualTo,
-            [Description("eq")]
-            Equals,
-            [Description("ne")]
-            NotEquals,
-            [Description("neq")]
-            NotEqualTo,
-            [Description("null")]
-            DoesNotContainData,
-            [Description("not-null")]
-            ContainsData,
-            [Description("in")]
-            IsIn,
-            [Description("not-in")]
-            IsNotIn,
-            [Description("between")]
-            Between,
-            [Description("not-between")]
-            IsNotBetween,
-            [Description("like")]
-            Like,
-            [Description("not-like")]
-            NotLike,
-            [Description("yesterday")]
-            Yesterday,
-            [Description("today")]
-            Today,
-            [Description("tomorrow")]
-            Tomorrow,
-            [Description("next-seven-days")]
-            NextSevenDays,
-            [Description("last-seven-days")]
-            LastSevenDays,
-            [Description("next-week")]
-            NextWeek,
-            [Description("last-week")]
-            LastWeek,
-            [Description("this-month")]
-            ThisMonth,
-            [Description("last-month")]
-            LastMonth,
-            [Description("next-month")]
-            NextMonth,
-            [Description("on")]
-            On,
-            [Description("on-or-before")]
-            OnorBefore,
-            [Description("on-or-after")]
-            OnonAfter,
-            [Description("this-year")]
-            ThisYear,
-            [Description("last-year")]
-            LastYear,
-            [Description("next-year")]
-            NextYear,
-            [Description("eq-userid")]
-            EqualsCurrentUser,
-            [Description("ne-userid")]
-            DoesNotEqualCurrentUser,
-            [Description("eq-businessid")]
-            EqualsCurrentBusinessUnit,
-            [Description("ne-businessid")]
-            DoesNotEqualCurrentBusinessUnit,
-            [Description("this-week")]
-            ThisWeek,
-            [Description("last-x-months")]
-            LastXMonths,
-            [Description("eq-userlanguage")]
-            EqualsUserLanguage,
-            [Description("eq-userteams")]
-            EqualsCurrentUsersTeams,
-            [Description("in-fiscal-year")]
-            InFiscalYear,
-            [Description("in-fiscal-period")]
-            InFiscalPeriod,
-            [Description("in-fiscal-period-and-year")]
-            InFiscalPeriodandYear,
-            [Description("in-or-after-fiscal-period-and-year")]
-            InorAfterFiscalPeriodandYear,
-            [Description("in-or-before-fiscal-period-and-year")]
-            InorBeforeFiscalPeriodandYear,
-            [Description("last-fiscal-year")]
-            LastFiscalYear,
-            [Description("this-fiscal-year")]
-            ThisFiscalYear,
-            [Description("next-fiscal-year")]
-            NextFiscalYear,
-            [Description("last-x-fiscal-years")]
-            LastXFiscalYears,
-            [Description("next-x-fiscal-years")]
-            NextXFiscalYears,
-            [Description("last-fiscal-period")]
-            LastFiscalPeriod,
-            [Description("this-fiscal-period")]
-            ThisFiscalPeriod,
-            [Description("next-fiscal-period")]
-            NextFiscalPeriod,
-            [Description("last-x-fiscal-periods")]
-            LastXFiscalPeriods,
-            [Description("next-x-fiscal-periods")]
-            NextXFiscalPeriods
-        }
-        #endregion
-
         #endregion
 
         public string ConvertToSQL(string FetchXML)
         {
             try
             {
-                // TO DO - Validate FetchXML
-
-                if (false)
+                if (!IsValidXml(FetchXML))
                 {
                     throw new Exception("Invalid FetchXML!");
                 }
@@ -256,8 +178,8 @@ namespace FetchXMLToSql
                 string page = string.Empty;
                 string orderBy = string.Empty;
                 string where = string.Empty;
+                string join = string.Empty;
                 #endregion
-
 
                 if (fetch.Count.HasValue)
                 {
@@ -285,33 +207,23 @@ namespace FetchXMLToSql
                     columns = "*";
                 }
 
-                if (fetch.Entity.Order != null && fetch.Entity.Order.Any())
+                orderBy = ConvertToSqlOrderByClause(fetch.Entity.Order);
+
+                where = ConvertToSqlWhereClause(fetch.Entity.Filter);
+
+                if (fetch.Entity.LinkEntity != null && fetch.Entity.LinkEntity.Any())
                 {
-                    orderBy = $"ORDER BY ";
+                    string alias = "t0";
 
-                    for (int i = 0; i < fetch.Entity.Order.Count() - 1; i++)
-                    {
-                        string direction = string.Empty;
+                    if (!string.IsNullOrEmpty(fetch.Entity.Alias))
+                        alias = fetch.Entity.Alias;
 
-                        if (!string.IsNullOrEmpty(fetch.Entity.Order[i].Descending))
-                        {
-                            direction = fetch.Entity.Order[i].Descending == "false" ? "ASC" : "DESC";
-                        }
+                    join = ConvertToSqlJoin(fetch.Entity.LinkEntity, alias);
 
-                        orderBy += $"{fetch.Entity.Order[i].Attribute} {direction}";
-
-                        if (i + 1 < fetch.Entity.Order.Count())
-                            orderBy += " ,";
-                    }
+                    table = ""; // the method above insert alias in the join statement
                 }
 
-                // Order By
-
-                // Filter
-
-                // Link Entities
-
-                string sql = $"SELECT {top} {columns} FROM {table} {where} {orderBy} {page}";
+                string sql = $"SELECT {top} {columns} FROM {table} {join} {where} {orderBy} {page}";
 
                 return sql;
             }
@@ -322,6 +234,189 @@ namespace FetchXMLToSql
         }
 
         #region [Privates]
+        private string ConvertToSqlOrderByClause(List<Order> Order)
+        {
+            string orderBy = string.Empty;
+
+            if (Order == null || !Order.Any())
+            {
+                return orderBy;
+            }
+
+            orderBy = $"ORDER BY ";
+
+            for (int i = 0; i < Order.Count() - 1; i++)
+            {
+                string direction = string.Empty;
+
+                if (!string.IsNullOrEmpty(Order[i].Descending))
+                {
+                    direction = Order[i].Descending == "false" ? "ASC" : "DESC";
+                }
+
+                orderBy += $"{Order[i].Attribute} {direction}";
+
+                if (i + 1 < Order.Count())
+                    orderBy += " ,";
+            }
+
+            return orderBy;
+        }
+
+        private string ConvertToSqlWhereClause(Filter filter)
+        {
+            if (filter == null)
+            {
+                return string.Empty;
+            }
+
+            StringBuilder whereClause = new StringBuilder();
+
+            if (filter.Condition != null && filter.Condition.Count > 0)
+            {
+                foreach (var condition in filter.Condition)
+                {
+                    if (whereClause.Length > 0)
+                    {
+                        whereClause.Append($" {filter.Type} ");
+                    }
+
+                    whereClause.Append("(");
+                    whereClause.Append($"{condition.Attribute} {GetSqlOperator(condition.Operator)}");
+
+                    if (condition.Value != null && condition.Value.Count > 0)
+                    {
+                        whereClause.Append("(");
+
+                        foreach (var value in condition.Value)
+                        {
+                            whereClause.Append($"'{value}'");
+                            if (value != condition.Value.Last())
+                            {
+                                whereClause.Append(" OR ");
+                            }
+                        }
+
+                        whereClause.Append(")");
+                    }
+
+                    whereClause.Append(")");
+                }
+            }
+
+            if (filter.InnerFilter != null)
+            {
+                if (whereClause.Length > 0)
+                {
+                    whereClause.Insert(0, "(");
+                    whereClause.Append(")");
+                }
+
+                string innerFilterClause = ConvertToSqlWhereClause(filter.InnerFilter);
+
+                if (!string.IsNullOrEmpty(innerFilterClause))
+                {
+                    if (whereClause.Length > 0)
+                    {
+                        whereClause.Append($" {filter.Type} ");
+                    }
+                    whereClause.Append(innerFilterClause);
+                }
+            }
+
+            return "WHERE " + whereClause.ToString();
+        }
+
+        private string GetSqlOperator(string fetchXmlOperator)
+        {
+            switch (fetchXmlOperator.ToLower())
+            {
+                case "eq":
+                    return "=";
+                case "neq":
+                    return "<>";
+                case "gt":
+                    return ">";
+                case "ge":
+                    return ">=";
+                case "lt":
+                    return "<";
+                case "le":
+                    return "<=";
+                case "like":
+                    return "LIKE";
+                default:
+                    throw new NotSupportedException($"Operator '{fetchXmlOperator}' is not supported.");
+            }
+        }
+
+        private string ConvertToSqlJoin(List<LinkEntity> linkEntities, string primaryTableAlias = "t0")
+        {
+            var joinStringBuilder = new StringBuilder();
+
+            joinStringBuilder.Append($"FROM {linkEntities[0].Name} AS {primaryTableAlias}");
+
+            var aliasCounters = new Dictionary<string, int> { { primaryTableAlias, 0 } };
+
+            for (int i = 1; i < linkEntities.Count; i++)
+            {
+                var linkEntity = linkEntities[i];
+                string parentAlias = GetTableAlias(linkEntity.From, aliasCounters, primaryTableAlias);
+                string childAlias = GetTableAlias(linkEntity.Name, aliasCounters);
+
+                switch (linkEntity.LinkType)
+                {
+                    case "outer":
+                        joinStringBuilder.Append($" LEFT JOIN {linkEntity.Name} AS {childAlias} " +
+                                                 $"ON {parentAlias}.{linkEntity.To} = {childAlias}.{linkEntity.From}");
+                        break;
+                    case "inner":
+                    default:
+                        joinStringBuilder.Append($" INNER JOIN {linkEntity.Name} AS {childAlias} " +
+                                                 $"ON {parentAlias}.{linkEntity.To} = {childAlias}.{linkEntity.From}");
+                        break;
+                }
+            }
+
+            return joinStringBuilder.ToString();
+        }
+
+        private string GetTableAlias(string tableName, Dictionary<string, int> aliasCounters, string primaryTableAlias = null)
+        {
+            string aliasBase = tableName.ToLowerInvariant();
+            if (aliasCounters.TryGetValue(aliasBase, out int aliasCounter))
+            {
+                aliasCounters[aliasBase]++;
+                return $"{aliasBase}{aliasCounter}";
+            }
+
+            aliasCounters[aliasBase] = 1;
+            return aliasBase;
+        }
+
+        private bool IsValidXml(string xmlString)
+        {
+            try
+            {
+                XmlReaderSettings settings = new XmlReaderSettings();
+
+                settings.ValidationType = ValidationType.None;
+
+                using (XmlReader reader = XmlReader.Create(new System.IO.StringReader(xmlString), settings))
+                {
+                    while (reader.Read())
+                    {
+                    }
+                }
+
+                return true;
+            }
+            catch (XmlException)
+            {
+                return false;
+            }
+        }
+
         private Fetch DeserializeXML(string xml)
         {
             try
